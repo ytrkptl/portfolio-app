a93a02efbb519d9b50c6b4379a7dcd8d4a1e644b
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2019-09-08 13:59:51 -0400

    removed Equations folder and changed teacher to dev image, reverted everything else back

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import teacherToDev from '../../images/teacher11.png';
+import teacherToDev from '../../images/teacherToDev.png';
 import ButtonInCard from '../Button-in-card/Button-in-card';
 import ProfilePhoto from '../../images/profile-photo-2.jpg';
-import Equation from '../Equation/Equation';
 import './Banner.css';
 
 const Banner = () => {
@@ -17,16 +16,12 @@ const Banner = () => {
 				</h1>
 				<img alt="profile" src={ProfilePhoto} className="profilePhoto"/>
 				<h3 className="belowNameText">
-					Math teacher passionate about becoming a developer.
+					Math teacher passionate about becoming a developer
 				</h3>
-				<h3 className="greenText">
-					I guess the best way to express that is with the following:
+				<h3 className="aboveFunctionText">
+					I guess the best way to express that is with the following items:
 				</h3>
-				<p className="aboveEquationText">
-					You get three trys for each equation. Your score will be 
-					calculated based on the number of different questions you've tried answering.
-				</p>
-				<Equation />
+				<img alt="teacher to developer function" src={teacherToDev} className="teacherToDevImage"/>
 				<ButtonInCard nextCardName='aboutme' />
 			</div>
 		</div>
