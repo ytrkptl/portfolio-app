0b90c3cc5062cb957e467c9853c70b144f392cb2
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-08-21 19:42:33 -0400

    removed scholarships

--- src/components/IndividualCards/Developer/Developer.js
+++ src/components/IndividualCards/Developer/Developer.js
@@ -1,39 +1,58 @@
-import React from 'react';
-import CustomCard3 from '../../Card-wrapper/CustomCard3';
-import ButtonInCard from '../../Button-in-card/Button-in-card'
+import React from "react";
+import CustomCard3 from "../../Card-wrapper/CustomCard3";
+import ButtonInCard from "../../Button-in-card/Button-in-card";
 
-import './Developer.css';
+import "./Developer.css";
 
-let skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js', 'Express.js',
-	'XML', 'Java', 'Android', 'Git', 'GitHub', 'AWS EC2', 'AWS Lambda', 'Serverless',
-	'PostgreSQL', 'SQLite3', 'Firebase', 'Parse Server', 'Docker', 'Redis', 'REST API',
-	'Gatsby', 'Next.js', 'Python', 'Flask']
-let online = ['Grow With Google Challenge Scholarship Recipient', 'Bertelsmann Technology Scholarship']
+let skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node.js",
+  "Express.js",
+  "MongoDB",
+  "XML",
+  "Java",
+  "Android",
+  "Git",
+  "GitHub",
+  "AWS EC2",
+  "AWS Lambda",
+  "Serverless",
+  "PostgreSQL",
+  "SQLite3",
+  "Firebase",
+  "Parse Server",
+  "Docker",
+  "Redis",
+  "REST API",
+  "Gatsby",
+  "Next.js",
+  "Python",
+  "Flask",
+];
 
 const Developer = ({ title, id, cardNum }) => {
-	return (
-		<CustomCard3 id={id} cardNum={cardNum} title={title}>
-			<h3 className="devSubtitle">
-				I've built my projects with the following, i.e. have experience using the following:
-				</h3>
-			<div className="devPillParent">
-				{
-					skills.map((el, index) => {
-						return <span key={index} className="devPill">{el}</span>
-					})
-				}
-			</div>
-			<h3 className="devSubtitle">Online Scholarships:</h3>
-			<div className="devPillParent">
-				{
-					online.map((el, index) => {
-						return <p key={index} className="devPill">{el}</p>
-					})
-				}
-			</div>
-			<ButtonInCard nextCardName='projects' bgColor='pink' />
-		</CustomCard3>
-	);
-}
+  return (
+    <CustomCard3 id={id} cardNum={cardNum} title={title}>
+      <h3 className="devSubtitle">
+        I've built my projects with the following, i.e. have experience using
+        the following:
+      </h3>
+      <div className="devPillParent">
+        {skills.map((el, index) => {
+          return (
+            <span key={index} className="devPill">
+              {el}
+            </span>
+          );
+        })}
+      </div>
+      <ButtonInCard nextCardName="projects" bgColor="pink" />
+    </CustomCard3>
+  );
+};
 
 export default Developer;
