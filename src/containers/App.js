35d01143ce023f4c02eaaf27bb2cdbe46ea921a9
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-20 22:33:46 -0500

    prettified

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,54 +1,59 @@
-import React, { Component, createRef } from 'react';
-import Navigation from '../components/Navigation/Navigation';
-import Banner from '../components/Banner/Banner';
-import CardList from '../components/CardList/CardList';
-import Footer from '../components/Footer/Footer';
-import arrow from '../assets/arrow2.png';
-import ReactGA from 'react-ga';
-import './App.css'
+import React, { Component, createRef } from "react";
+import Navigation from "../components/Navigation/Navigation";
+import Banner from "../components/Banner/Banner";
+import CardList from "../components/CardList/CardList";
+import Footer from "../components/Footer/Footer";
+import arrow from "../assets/arrow2.png";
+import ReactGA from "react-ga";
+import "./App.css";
 
 ReactGA.initialize(`${process.env.REACT_APP_GA_TRACKING_ID}`);
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-// the following function accepts hashname or id 
+// the following function accepts hashname or id
 // and scrolls to that id's location on the page
-export const scrollTo = hashName => {
+export const scrollTo = (hashName) => {
   let parentRect = document.body.getBoundingClientRect();
   let element = document.getElementById(hashName);
   let rect = element.getBoundingClientRect();
   let offset = rect.top - parentRect.top - 20;
   window.scrollTo(0, offset);
-}
+};
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      route: 'home',
-      hamburgerClicked: false
-    }
+      input: "",
+      route: "home",
+      hamburgerClicked: false,
+      currentRole: "teacher",
+      animState: 0,
+    };
     this.rootDivRef = createRef();
   }
 
   componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  };
+    window.addEventListener("scroll", this.handleScroll);
+  }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  };
+    window.removeEventListener("scroll", this.handleScroll);
+  }
 
   // this function moves the scrollToTop Button a little higher
-  // when the user scrolls the the very bottom of the page
-  // so it doesn't appear over the footer.
+  // when the user scrolls to the very bottom of the page
+  // so it doesn't overlap the footer.
   checkIfScrollIsAtBottom = () => {
-    if (window.scrollY + window.innerHeight >= (document.documentElement.scrollHeight - 60)) {
+    if (
+      window.scrollY + window.innerHeight >=
+      document.documentElement.scrollHeight - 60
+    ) {
       document.getElementById("floatingBtn").style.bottom = "66px";
     } else {
       document.getElementById("floatingBtn").style.bottom = "30px";
     }
-  }
+  };
 
   // show the move to top scroll button when the user scroll down.
   handleScroll = (event) => {
@@ -65,22 +70,38 @@ class App extends Component {
   // if the overlay icon is displayed, close the overlay, then
   // scroll to top.  Otherwise, simply scroll to top.
   scrollToTop = () => {
-    let overlay = document.getElementById('overlay-content');
+    let overlay = document.getElementById("overlay-content");
     let overlayHeight = overlay.clientHeight;
-    let toggler = document.getElementById('hamburgerRef');
+    let toggler = document.getElementById("hamburgerRef");
     if (overlayHeight !== 0) {
       toggler.click();
     }
     window.scrollTo(0, 0);
-  }
+  };
 
-  // use this function to scroll to specified hashname from 
+  // use this function to scroll to specified hashname from
   // hamburger icon/overlay menu.
   scrollToFromMenu = (hashName) => {
-    let toggler = document.getElementById('hamburgerRef');
+    let toggler = document.getElementById("hamburgerRef");
     toggler.click();
     scrollTo(hashName);
-  }
+  };
+
+  // function to update the role
+  updateRole = (role) => {
+    this.setState({ currentRole: role });
+    switch (role) {
+      case "teacher":
+        this.setState({ animState: 1 });
+        break;
+      case "developer":
+        this.setState({ animState: 2 });
+        break;
+      default:
+        this.setState({ animState: 0 });
+        break;
+    }
+  };
 
   render() {
     return (
@@ -88,19 +109,20 @@ class App extends Component {
         <Navigation
           scrollToFromMenu={this.scrollToFromMenu}
           scrollToTop={this.scrollToTop}
-          toggleFunc={this.toggleFunc} />
-
+          toggleFunc={this.toggleFunc}
+        />
         <div className="AppDiv">
-          <Banner scrollTo={this.scrollTo} />
+          <Banner scrollTo={this.scrollTo} animState={this.state.animState} />
           <div className="cardListParent">
-            <CardList scrollTo={this.scrollTo} />
+            <CardList
+              scrollTo={this.scrollTo}
+              currentRole={this.state.currentRole}
+            />
           </div>
           <Footer />
         </div>
         <div id="floatingBtn" onClick={this.scrollToTop}>
-          <img id="arrow"
-            src={arrow}
-            alt="move to top" />
+          <img id="arrow" src={arrow} alt="move to top" />
         </div>
       </div>
     );
