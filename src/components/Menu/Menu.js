e50ea1eb0301cf54579760d1a9ddf2c3026c772b
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-20 22:28:46 -0500

    prettified

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,49 +1,51 @@
-import React from 'react';
-import './Menu.css';
-import './Hamburger.css';
-import { Data } from '../CardList/Data';
+import React from "react";
+import "./Menu.css";
+import "./Hamburger.css";
+import { Data } from "../CardList/Data";
 
 const Menu = ({ scrollToFromMenu }) => {
-	const hamburgerRef = React.createRef();
-	const menuRef = React.createRef();
+  const hamburgerRef = React.createRef();
+  const menuRef = React.createRef();
 
-	const toggleFunc = () => {
-		hamburgerRef.current.classList.toggle("change");
-		if (hamburgerRef.current.className === "hamburgerContainer") {
-			closeNav();
-		} else {
-			openNav();
-		}
-	}
-	const openNav = () => {
-		menuRef.current.style.height = "100%";
-	}
-	const closeNav = () => {
-		menuRef.current.style.height = "0%";
-	}
-	return (
-		<div id="menu">
-			<div ref={menuRef} id="myNav" className="overlay">
-				<div className="overlay-content" id="overlay-content">
-					{Data.map((el) => (
-						<button
-							key={el.id}
-							id={`${el.id}Btn2`}
-							className="col3Btns grow"
-							onClick={() => scrollToFromMenu(el.id)}>{el.name}
-						</button>)
-					)}
-				</div>
-			</div>
-			<span onClick={toggleFunc} id="hamburgerRef">
-				<div ref={hamburgerRef} className="hamburgerContainer">
-					<div className="bar1" />
-					<div className="bar2" />
-					<div className="bar3" />
-				</div>
-			</span>
-		</div>
-	);
-}
+  const toggleFunc = () => {
+    hamburgerRef.current.classList.toggle("change");
+    if (hamburgerRef.current.className === "hamburgerContainer") {
+      closeNav();
+    } else {
+      openNav();
+    }
+  };
+  const openNav = () => {
+    menuRef.current.style.height = "100%";
+  };
+  const closeNav = () => {
+    menuRef.current.style.height = "0%";
+  };
+  return (
+    <div id="menu">
+      <div ref={menuRef} id="myNav" className="overlay">
+        <div className="overlay-content" id="overlay-content">
+          {Data.map((el) => (
+            <button
+              key={el.id}
+              id={`${el.id}Btn2`}
+              className="col3Btns grow"
+              onClick={() => scrollToFromMenu(el.id)}
+            >
+              {el.name}
+            </button>
+          ))}
+        </div>
+      </div>
+      <span onClick={toggleFunc} id="hamburgerRef">
+        <div ref={hamburgerRef} className="hamburgerContainer">
+          <div className="bar1" />
+          <div className="bar2" />
+          <div className="bar3" />
+        </div>
+      </span>
+    </div>
+  );
+};
 
 export default Menu;
