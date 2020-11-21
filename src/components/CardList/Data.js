345170141581f5d67d067ebe4b2e3208c424d777
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-20 22:02:11 -0500

    updated Data.js with Teacher and Blog

--- src/components/CardList/Data.js
+++ src/components/CardList/Data.js
@@ -1,9 +1,10 @@
 // Put all larger strings and data here for exporting.
 export const Data = [
-  { name: 'About Me', id: 'aboutme'},
-  { name: 'Developer', id: 'developer'},
-	{ name: 'Projects', id: 'projects'},
-	{ name: 'Contact', id: 'contact'},
-  { name: 'Credits', id: 'credits'}
+  { name: "About Me", id: "aboutme" },
+  { name: "Projects", id: "projects" },
+  { name: "Developer", id: "developer" },
+  { name: "Blog", id: "blog" },
+  { name: "Teacher", id: "teacher" },
+  { name: "Contact", id: "contact" },
+  { name: "Credits", id: "credits" },
 ];
-
