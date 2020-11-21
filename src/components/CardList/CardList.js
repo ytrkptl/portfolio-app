e3bb9f60b786e9fcae0a75a8dd3d2d9f33d02829
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-20 22:01:52 -0500

    added Blog and Teacher cards

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,34 +1,22 @@
-import React from 'react';
-import AboutMe from '../IndividualCards/AboutMe/AboutMe';
-import Developer from '../IndividualCards/Developer/Developer';
-import Projects from '../IndividualCards/Projects/Projects';
-import Contact from '../IndividualCards/Contact/Contact';
-import Credits from '../IndividualCards/Credits/Credits';
+import React from "react";
+import AboutMe from "../IndividualCards/AboutMe/AboutMe";
+import Developer from "../IndividualCards/Developer/Developer";
+import Blog from "../IndividualCards/Blog/Blog";
+import Teacher from "../IndividualCards/Teacher/Teacher";
+import Projects from "../IndividualCards/Projects/Projects";
+import Contact from "../IndividualCards/Contact/Contact";
+import Credits from "../IndividualCards/Credits/Credits";
 
-import { Data } from './Data.js';
+import { Data } from "./Data.js";
 
 const CardList = ({ scrollTheRoot }) => {
-  let itemsArr = []
+  let itemsArr = [];
   for (let i = 0; i < Data.length; i++) {
     if (i === 0) {
       itemsArr.push(
-        <AboutMe
-          title={Data[i].name}
-          key={i}
-          id={Data[i].id}
-          cardNum={i}
-        />
-      )
+        <AboutMe title={Data[i].name} key={i} id={Data[i].id} cardNum={i} />
+      );
     } else if (i === 1) {
-      itemsArr.push(
-        <Developer
-          title={Data[i].name}
-          key={i}
-          id={Data[i].id}
-          cardNum={i}
-        />
-      )
-    } else if (i === 2) {
       itemsArr.push(
         <Projects
           title={Data[i].name}
@@ -37,28 +25,30 @@ const CardList = ({ scrollTheRoot }) => {
           cardNum={i}
           scrollTheRoot={scrollTheRoot}
         />
-      )
+      );
+    } else if (i === 2) {
+      itemsArr.push(
+        <Developer title={Data[i].name} key={i} id={Data[i].id} cardNum={i} />
+      );
     } else if (i === 3) {
       itemsArr.push(
-        <Contact
-          title={Data[i].name}
-          key={i}
-          id={Data[i].id}
-          cardNum={i}
-        />
-      )
+        <Blog title={Data[i].name} key={i} id={Data[i].id} cardNum={i} />
+      );
+    } else if (i === 4) {
+      itemsArr.push(
+        <Teacher title={Data[i].name} key={i} id={Data[i].id} cardNum={i} />
+      );
+    } else if (i === 5) {
+      itemsArr.push(
+        <Contact title={Data[i].name} key={i} id={Data[i].id} cardNum={i} />
+      );
     } else {
       itemsArr.push(
-        <Credits
-          title={Data[i].name}
-          key={i}
-          id={Data[i].id}
-          cardNum={i}
-        />
-      )
+        <Credits title={Data[i].name} key={i} id={Data[i].id} cardNum={i} />
+      );
     }
   }
-  return itemsArr
-}
+  return itemsArr;
+};
 
 export default CardList;
