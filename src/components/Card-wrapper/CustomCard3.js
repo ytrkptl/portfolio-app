60546af6b53f436ad5d1c0f0bbebcc8abc2447b4
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-20 22:00:43 -0500

    prettified

--- src/components/Card-wrapper/CustomCard3.js
+++ src/components/Card-wrapper/CustomCard3.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './CustomCard.css';
+import React from "react";
+import "./CustomCard.css";
 
 const CustomCard3 = ({ title, id, children }) => {
-	return (
-		<div className="CardDetails" id={id}>
-			<div className="textContainer">
-				<h2 className="subjectTitle">{title}</h2>
-				{children}
-			</div>
-		</div>
-	);
-}
+  return (
+    <div className="CardDetails" id={id}>
+      <div className="textContainer">
+        <h2 className="subjectTitle">{title}</h2>
+        {children}
+      </div>
+    </div>
+  );
+};
 
 export default CustomCard3;
