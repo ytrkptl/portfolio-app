f8d830974f166a56f79fad564025af964f8eab63
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2021-02-07 08:15:01 -0500

    added link to Dev.to

--- src/components/IndividualCards/Blog/Blog.js
+++ src/components/IndividualCards/Blog/Blog.js
@@ -7,13 +7,21 @@ const Blog = ({ title, id, cardNum }) => {
   return (
     <CustomCard3 id={id} cardNum={cardNum} title={title}>
       <p className="blogPara1">
-        For now, you can read my blog on{" "}
+        For now, you can read my blogs on either{" "}
         <a
           href="https://yatrik.hashnode.dev/"
           target="_blank"
           rel="noopener noreferrer"
         >
           Hashnode
+        </a>{" "}
+        or{" "}
+        <a
+          href="https://dev.to/ytrkptl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dev.to
         </a>
       </p>
       <p className="blogPara1">Will be adding more to this card soon</p>
