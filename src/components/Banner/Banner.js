c7c7b550c9a07d6bd422d23d412ecfd868801f51
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2021-03-21 12:03:33 -0400

    updated styles for better semantics and performance

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,16 +6,16 @@ const Banner = () => {
   return (
     <div className="imageContainerParent" id="banner">
       <div className="imageContainer">
-        <h2 className="aboveNameText">Meet Me</h2>
+        <span className="aboveNameText">Meet Me</span>
         <h1 className="nameText">Yatrik Patel</h1>
         <img
           alt="profile"
           src="https://res.cloudinary.com/dun1b4fpw/image/upload/c_limit,f_auto,h_140,q_auto,w_140/v1574951668/portfolioapp/profile.jpg"
           className="profilePhoto"
         />
-        <h3 className="belowNameText">
+        <h2 className="belowNameText">
           Math teacher passionate about becoming a developer
-        </h3>
+        </h2>
         <h3 className="aboveFunctionText">
           I guess the best way to express that is with the following:
         </h3>
