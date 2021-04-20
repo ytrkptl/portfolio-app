d5f05af09cc513c0eb60b93d86a74600081ee54d
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2021-04-19 21:42:10 -0400

    renamed some values and added url key and value

--- src/components/CardList/Data.js
+++ src/components/CardList/Data.js
@@ -1,10 +1,10 @@
 // Put all larger strings and data here for exporting.
 export const Data = [
-  { name: "About Me", id: "aboutme" },
-  { name: "Projects", id: "projects" },
-  { name: "Developer", id: "developer" },
-  { name: "Blog", id: "blog" },
-  { name: "Teacher", id: "teacher" },
-  { name: "Contact", id: "contact" },
-  { name: "Credits", id: "credits" },
+  { name: "About Me", id: "about-me", url: "about-me" },
+  { name: "Projects", id: "projects",  url: "projects" },
+  { name: "Developer", id: "developer",  url: "developer" },
+  { name: "Blog", id: "blog-info",  url: "blog-info" },
+  { name: "Teacher", id: "teacher",  url: "teacher" },
+  { name: "Contact", id: "contact",  url: "contact" },
+  { name: "Credits", id: "credits",  url: "credits" },
 ];
