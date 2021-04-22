4282489f9e6b62a85b1b6b12169f010f17a6e8bc
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2021-04-21 20:44:31 -0400

    added BlogListData

---
+++ src/components/BlogList/BlogListData.js
@@ -0,0 +1,18 @@
+// keep all BlogList Data in here
+export const BlogListData = [
+  { title: "Avatar Letters",
+    subtitle: "Looking for some feedback on this Avatar-letter project I thought of sharing months ago", 
+    excerpt: "Adipisicing sunt duis ex sunt sit esse esse. Tempor enim laboris aute ex magna. Do culpa nostrud ipsum culpa esse id id dolor cillum cillum esse laborum dolore amet. Ullamco cupidatat cupidatat excepteur velit commodo dolore commodo veli",
+    url: "avatar-letters"
+  },
+  { title: "SVG Ugly Christmas Sweaters",
+    subtitle: "Having fun creating Ugly Christmas Sweaters with SVGs this Winter", 
+    excerpt: "Adipisicing sunt duis ex sunt sit esse esse. Tempor enim laboris aute ex magna. Do culpa nostrud ipsum culpa esse id id dolor cillum cillum esse laborum dolore amet. Ullamco cupidatat cupidatat excepteur velit commodo dolore commodo veli",
+    url: "svg-ugly-christmas-sweaters"
+  },
+  { title: "Zoom Chat Participation Counter",
+    subtitle: "Count Participation In Your Zoom Meetings The Easier Way", 
+    excerpt: "Adipisicing sunt duis ex sunt sit esse esse. Tempor enim laboris aute ex magna. Do culpa nostrud ipsum culpa esse id id dolor cillum cillum esse laborum dolore amet. Ullamco cupidatat cupidatat excepteur velit commodo dolore commodo veli",
+    url: "zoom-chat-participation-counter"
+  },
+];
