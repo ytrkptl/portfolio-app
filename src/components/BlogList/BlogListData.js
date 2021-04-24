3d7daae5c01b7dbac937c8d67978cfa4b822ad9f
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2021-04-23 20:44:55 -0400

    updated BlogListData

--- src/components/BlogList/BlogListData.js
+++ src/components/BlogList/BlogListData.js
@@ -2,7 +2,7 @@
 export const BlogListData = [
   { title: "Avatar Letters",
     subtitle: "Looking for some feedback on this Avatar-letter project I thought of sharing months ago", 
-    excerpt: "Adipisicing sunt duis ex sunt sit esse esse. Tempor enim laboris aute ex magna. Do culpa nostrud ipsum culpa esse id id dolor cillum cillum esse laborum dolore amet. Ullamco cupidatat cupidatat excepteur velit commodo dolore commodo veli",
+    excerpt: "Over the last 2-3 years, in my free time, I've been lerning web and mobile development online. During that time, I've created several projects that live either on my computer, online, or both. These include projects made to solve a problem I encountered while working (for both past and current employers), tutorials I've followed while learning something new, and projects I'm passionate about. However, I've never really created a service or an API that is open to the public for use, and/or haven't created any open source projects myself. So, I wanted to give this a shot.",
     url: "avatar-letters"
   },
   { title: "SVG Ugly Christmas Sweaters",
