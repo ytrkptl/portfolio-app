7c9e8e2c82f62b637b32bba2ea67bbfc30719267
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2021-05-02 10:24:38 -0400

    removed usage of useHistory and ScrollToTop functions; the scrollToTop button should suffice in most cases

--- src/containers/MdxContainer.js
+++ src/containers/MdxContainer.js
@@ -2,25 +2,16 @@ import React from "react"
 import BlogList from "../components/BlogList/BlogList";
 import MdxPagesWithRoutes from "../components/MdxPagesWithRoutes/MdxPagesWithRoutes"
 import BlogHeaderWithRoutes from "../components/BlogHeaderWithRoutes/BlogHeaderWithRoutes";
-import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
-import { Switch, Route, Link, useHistory } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import "./MdxContainer.css"
 
 const BackLink = () => <span className="back-link">&#10141;</span>
 
 const BackLinkSwitchFunction = ({ containerType }) => {
 
-  let history = useHistory()
-
-  // to restore scroll to the last blog they saw on the blog page
-  const handleGoingBack = (e) => {
-    e.preventDefault();
-    history.push("/blog")
-  }
-
   switch (containerType) {
     case 2:
-      return <Link to="/blog" onClick={(e)=>handleGoingBack(e)}><BackLink />Back</Link>
+      return <Link to="/blog"><BackLink />Back</Link>
     default:
       return <Link to="/"><BackLink />Back</Link>
   }
@@ -38,7 +29,6 @@ const MdxContainer = () => {
             <BlogList  />
           </Route>
           <Route exact path="/blog/*">
-            <ScrollToTop />
             <BackLinkContainer containerType={2} />
             <BlogHeaderWithRoutes />
             <MdxPagesWithRoutes />
