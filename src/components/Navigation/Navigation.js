e66c5d031acc292e61010a228934424a8208d8f6
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2021-05-02 10:10:33 -0400

    removed usage of hamburgerClicked and converted from class component to functional component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,62 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
 import Logo from "../Logo/Logo";
 import Menu from "../Menu/Menu";
 import { Data } from "../CardList/Data";
 import "./Navigation.css";
 import { Link } from "react-router-dom";
 
-class Navigation extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hamburgerClicked: this.props.hamburgerClicked,
-    };
-  }
+const Navigation = ({ scrollToTop, scrollToFromMenu }) => {
+  const handleClick = (fnName, id = null) => {
+    fnName === "scrollToTop" ? scrollToTop() : scrollToFromMenu(id);
+  };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.hamburgerClicked !== this.props.hamburgerClicked) {
-      this.setState({ hamburgerClicked: this.props.hamburgerClicked });
-    }
-  }
-
-  handleClick(fnName, id = null) {
-    fnName === "scrollToTop"
-      ? this.props.scrollToTop()
-      : this.props.scrollToFromMenu(id);
-  }
-
-  render() {
-    return (
-      <nav id="headerMainDiv">
-        <div id="col1">
-          <Logo />
-        </div>
-        <div id="col2">
+  return (
+    <nav id="headerMainDiv">
+      <div id="col1">
+        <Logo />
+      </div>
+      <div id="col2">
+        <Link
+          id="titleBtn"
+          className="grow"
+          to="/"
+          onClick={() => handleClick("scrollToTop", null)}
+        >
+          Yatrik's Portfolio
+        </Link>
+      </div>
+      <div id="col3">
+        {Data.map((el) => (
           <Link
-            id="titleBtn"
-            className="grow"
-            to="/"
-            onClick={() => this.handleClick("scrollToTop", null)}
+            key={el.id}
+            id={`${el.id}Btn`}
+            className="col3Btns grow"
+            to={`/${el.url}`}
+            onClick={() => handleClick("scrollToFromMenu", el.id)}
           >
-            Yatrik's Portfolio
+            {el.name}
           </Link>
-        </div>
-        <div id="col3">
-          {Data.map((el) => (
-            <Link
-              key={el.id}
-              id={`${el.id}Btn`}
-              className="col3Btns grow"
-              to={`/${el.url}`}
-              onClick={() => this.handleClick("scrollToFromMenu", el.id)}
-            >
-              {el.name}
-            </Link>
-          ))}
-          <Menu scrollToFromMenu={this.props.scrollToFromMenu} />
-        </div>
-      </nav>
-    );
-  }
-}
-export default Navigation
+        ))}
+        <Menu scrollToFromMenu={scrollToFromMenu} />
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
