09ded576c187c7e38da271ff5d982493ad3a285b
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2021-05-02 09:56:49 -0400

    changed blog-info to blog

--- src/components/CardList/Data.js
+++ src/components/CardList/Data.js
@@ -3,7 +3,7 @@ export const Data = [
   { name: "About Me", id: "about-me", url: "about-me" },
   { name: "Projects", id: "projects",  url: "projects" },
   { name: "Developer", id: "developer",  url: "developer" },
-  { name: "Blog", id: "blog-info",  url: "blog-info" },
+  { name: "Blog", id: "blog",  url: "blog" },
   { name: "Teacher", id: "teacher",  url: "teacher" },
   { name: "Contact", id: "contact",  url: "contact" },
   { name: "Credits", id: "credits",  url: "credits" },
